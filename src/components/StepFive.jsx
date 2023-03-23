1d6939664bb67c3bd7a2f8ce2d5ae076ee01b347
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-23 17:30:59 +0100

    added markup

--- src/components/StepFive.jsx
+++ src/components/StepFive.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import confirm from '../assets/images/icon-thank-you.svg'
 
 const StepFive = () => {
   return (
-    <div>StepFive</div>
+    <>
+        <div>
+            <img src={confirm} alt="icon" />
+            <h1>Thank you!</h1>
+            <p>Thanks for confirming your subscription! We hope you have fun using our platform. 
+                If you ever need support, please feel free to email us at support@loremgaming.com.
+            </p>
+        </div>
+    </>
   )
 }
 
