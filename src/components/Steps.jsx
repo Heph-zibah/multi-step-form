f7b867ae559539ac3184940e2279bc74bc42de6b
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-23 18:41:48 +0100

    created steps file

---
+++ src/components/Steps.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <>
+    <aside>
+        <div>
+            <button>1</button>
+            <div>
+                <p>step 1</p>
+                <h5>your info</h5>
+            </div>
+        </div>
+    </aside>
+    </>
+  )
+}
+
+export default Steps
