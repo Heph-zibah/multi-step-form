d6c823a47572de3d1eb60f896fd3eea635409dca
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-23 17:23:28 +0100

    added markup

--- src/components/StepFour.jsx
+++ src/components/StepFour.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const StepFour = () => {
   return (
-    <div>StepFour</div>
+    <>
+        <div>
+            <h1>Finishing up</h1>
+            <p>Double-check everything looks OK before confirming.</p>
+            <strong>Total (per month/year)</strong>
+            <div>
+                <button>
+                    Go back
+                </button>
+                <button>
+                    Confirm
+                </button>
+            </div>
+        </div>
+    </>
   )
 }
 
