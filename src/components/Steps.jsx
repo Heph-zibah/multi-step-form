9ff4705e9073cea81028e1804871c1b80e79d95a
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-23 18:54:19 +0100

    update file

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -11,6 +11,27 @@ const Steps = () => {
                 <h5>your info</h5>
             </div>
         </div>
+        <div>
+            <button>2</button>
+            <div>
+                <p>step 2</p>
+                <h5>select plans</h5>
+            </div>
+        </div>
+        <div>
+            <button>3</button>
+            <div>
+                <p>step 3</p>
+                <h5>add ons</h5>
+            </div>
+        </div>
+        <div>
+            <button>4</button>
+            <div>
+                <p>step 4</p>
+                <h5>summary</h5>
+            </div>
+        </div>
     </aside>
     </>
   )
