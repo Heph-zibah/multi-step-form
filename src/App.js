4ad6ce862fcd71b88c266aa096df5be903f62ba5
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-23 16:58:44 +0100

    added step two file path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import StepOne from "./components/StepOne";
+import StepTwo from "./components/StepTwo";
 
 function App() {
   return (
     <div className="">
       <StepOne/>
+      <StepTwo/>
     </div>
   );
 }
