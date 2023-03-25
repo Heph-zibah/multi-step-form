f2f461815839a13aa0d96714206fdf305ca0ca3e
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-25 15:04:58 +0100

    update markup

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -7,23 +7,17 @@ const StepOne = () => {
     <div className='steps__container'>
         <h1>{header[0].h1}</h1>
         <p>{header[0].p}</p>
-        <form action="">
-            <div className='formGroup__step--one'>
+        <form action="" className='form__container1'> 
                 <label htmlFor="name">Name</label>
                 <input type="text" placeholder='e.g. Stephen King' />
-            </div>
-            <div className='formGroup__step--one'>
                 <label htmlFor="email">Email Address</label>
                 <input type="email" placeholder='e.g. stephenking@lorem.com' />
-            </div>
-            <div className='formGroup__step--one'>
                 <label htmlFor="phoneNumber">Phone Number</label>
                 <input type="text" placeholder='e.g. +1 234 567 890'/>
-            </div>
         </form>
         
     </div>
-    <div>
+    <div className='steps__actions'>
         <button className='formBtn--step--one'>Next Step</button>
     </div>
    </>
