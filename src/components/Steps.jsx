f80d099ecfc1222d8f8c697fa6f5c2e7fc74919d
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-25 13:23:32 +0100

    update markup

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,37 +1,19 @@
 import React from 'react'
+import { informationsStep } from '../hooks/texts'
 
 const Steps = () => {
   return (
     <>
     <aside>
-        <div>
-            <button>1</button>
-            <div>
-                <p>step 1</p>
-                <h5>your info</h5>
-            </div>
+        {informationsStep.map((step) => {
+            return <div className='steps__btn'>
+                <button>{step.num}</button>
+                <div className='steps__btn--info'>
+                    <p>step {step.num}</p>
+                    <h5>{step.name}</h5>
+                </div>
         </div>
-        <div>
-            <button>2</button>
-            <div>
-                <p>step 2</p>
-                <h5>select plans</h5>
-            </div>
-        </div>
-        <div>
-            <button>3</button>
-            <div>
-                <p>step 3</p>
-                <h5>add ons</h5>
-            </div>
-        </div>
-        <div>
-            <button>4</button>
-            <div>
-                <p>step 4</p>
-                <h5>summary</h5>
-            </div>
-        </div>
+        })}
     </aside>
     </>
   )
