61129e078d7ec42e3c4cfb704ca0b9330d35685b
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-25 16:14:14 +0100

    added the container and actions classname

--- src/components/StepFour.jsx
+++ src/components/StepFour.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const StepFour = () => {
   return (
     <>
-        <div>
+        <div className='steps__container'>
             <h1>Finishing up</h1>
             <p>Double-check everything looks OK before confirming.</p>
             <strong>Total (per month/year)</strong>
-            <div>
+            <div className='steps__actions'>
                 <button>
                     Go back
                 </button>
