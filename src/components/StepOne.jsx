6272028da1bf208ef0c9b1c733f98fe310d3f6d3
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-25 14:17:49 +0100

    revamp markup

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
+import { header } from '../hooks/texts'
 
 const StepOne = () => {
   return (
    <>
-    <form action="" className='form__step--one'>
-        <h1>Personal info</h1>
-        <p>Please provide your name, email address, and phone number.</p>
-        <div className='formGroup__step--one'>
-            <label htmlFor="name">Name</label>
-            <input type="text" placeholder='e.g. Stephen King' />
-        </div>
-        <div className='formGroup__step--one'>
-            <label htmlFor="email">Email Address</label>
-            <input type="email" placeholder='e.g. stephenking@lorem.com' />
-        </div>
-        <div className='formGroup__step--one'>
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input type="text" placeholder='e.g. +1 234 567 890'/>
-        </div>
-        <button className='formBtn--step--one'>Next Step</button>
+    <div className='steps__container'>
+        <h1>{header[0].h1}</h1>
+        <p>{header[0].p}</p>
+        <form action="">
+            <div className='formGroup__step--one'>
+                <label htmlFor="name">Name</label>
+                <input type="text" placeholder='e.g. Stephen King' />
+            </div>
+            <div className='formGroup__step--one'>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" placeholder='e.g. stephenking@lorem.com' />
+            </div>
+            <div className='formGroup__step--one'>
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input type="text" placeholder='e.g. +1 234 567 890'/>
+            </div>
+        </form>
         
-    </form>
+    </div>
+    <div>
+        <button className='formBtn--step--one'>Next Step</button>
+    </div>
    </>
   )
 }
