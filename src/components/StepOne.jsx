d1bd0be67338aa1d9713f9e3601b6eb9a7e046c0
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-04-05 20:43:37 +0100

    update file

--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { header } from '../hooks/texts'
 
 const StepOne = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const handleSubmit = (e) => {
+   
+   
+  
+  }
   return (
    <>
     <div className='steps__container'>
         <h1>{header[0].h1}</h1>
         <p>{header[0].p}</p>
-        <form action="" className='form__container1'> 
+        <form action="" className='form__container1' onSubmit={handleSubmit}> 
                 <label htmlFor="name">Name</label>
-                <input type="text" placeholder='e.g. Stephen King' />
+                <input type="text" id='name' name='name' placeholder='e.g. Stephen King' value={name} onChange={(e) => setName(e.target.value)}/>
+                {name ? <p>name required</p> : null}
                 <label htmlFor="email">Email Address</label>
-                <input type="email" placeholder='e.g. stephenking@lorem.com' />
+                <input type="email" id='email'name='email' placeholder='e.g. stephenking@lorem.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <label htmlFor="phoneNumber">Phone Number</label>
-                <input type="text" placeholder='e.g. +1 234 567 890'/>
+                <input type="text" id='phoneNumber' name='phoneNumber' placeholder='e.g. +1 234 567 890' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
         </form>
         <div className='steps__actions'>
-       <button  className='btn__goBack' style={{visibility:'hidden'}}> Go back</button>
-        <button className='btn__next'>Next Step</button>
-    </div>
+          <button  className='btn__goBack' style={{visibility:'hidden'}}> Go back</button>
+          <button className='btn__next' onClick={handleSubmit}>Next Step</button>
+        </div>  
     </div>
     
    </>
   )
 }
 
-export default StepOne
+export default StepOne 
